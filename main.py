dea1ae115ac4c18cb1d3541a426edddf49761b42
Skyilz <74194554+skyilz@users.noreply.github.com>
2021-03-29 13:23:13 +0800

    增加对任务是否存在的判断，并在qq推送时增加时间，方便确认

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import re
 # 读取csv中id的值
 f = open('id.csv')
 data = f.readline()
-# print(data)
 # 我们仅仅需要数字，用正则取出数字id
 id = re.sub("\D", "", data)
 print(id)
 base_url = 'https://apiopen.jingdaka.com/user/get_theme?calendar_id=%s'%(id)
+# 抓包获取
 headers = {
     "User-Agent":
     "Mozilla/5.0 (Linux; Android 11; Mi 10 Pro Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.62 XWEB/2767 MMWEBSDK/201201 Mobile Safari/537.36 MMWEBID/4024 MicroMessenger/8.0.1.1841(0x28000151) Process/appbrand0 WeChat/arm64 Weixin NetType/4G Language/zh_CN ABI/arm64 MiniProgramEnv/android",
@@ -29,10 +29,6 @@ headers = {
 if __name__ == '__main__':
     request = requests.get(base_url, headers=headers)
     d = json.loads(request.content.decode('utf8'))
-    # print(d)
-    # print(d['data']['is_submited'])
-    sub = d['data']['is_submited']
-    # print(sub)
     if sub == 1:
         test = 'yes'
         print(test)
@@ -44,24 +40,34 @@ if __name__ == '__main__':
             idFile.write('%d' % id)
         print(id)
     else:
-        test = '今日英语虎哥未打卡，尽快打卡'
-        print(test)
-        id = int(id)
-        id += 1
-        with open('id.csv', 'w') as idFile:
-            idFile.write('%d' % id)
-        QmsgKey = "                         "
-        content = f"""{test}"""
-        data = {
-            "msg": content
-        }
-        url_send = "https://qmsg.zendee.cn/send/%s" % (QmsgKey)
-        try:
-            res = requests.post(url_send, data=data)
-            sucmsg = res.json()['success']
-            if sucmsg == True:
-                print("qq推送服务成功")
-            else:
-                print("qq推送服务失败")
-        except:
-            print("qq推送参数错误")
+        if d['data']['pc_content'] == '':
+            test = '今日没有任务'
+            print(test)
+        else:
+            test = '英语虎哥未打卡，尽快打卡'
+            print(test)
+            # 存入id
+            id = int(id)
+            id += 1
+            with open('id.csv', 'w') as idFile:
+                idFile.write('%d' % id)
+            # 正则匹配时间
+            sub = d['data']['is_submited']
+            real_time = re.match(r'\w{4}-(\w{2}-\w{2})', time)
+            realtime = real_time.group(1)
+            # qq推送
+            QmsgKey = "                         "
+            content = f"""{realtime}{test}"""
+            data = {
+                "msg": content
+            }
+            url_send = "https://qmsg.zendee.cn/send/%s" % (QmsgKey)
+            try:
+                res = requests.post(url_send, data=data)
+                sucmsg = res.json()['success']
+                if sucmsg == True:
+                    print("qq推送服务成功")
+                else:
+                    print("qq推送服务失败")
+            except:
+                print("qq推送参数错误")
