f7a17a549caa8651dec8a3a2d7471f570ba1fe5a
skyil <zhangzhi030109@gmail.com>
2022-03-14 20:09:42 +0800

    new!!

--- main.py
+++ main.py
@@ -1,107 +1,103 @@
-# 服务器版本
 import json
-import re
 import requests
-import time
+import datetime
+import random
+
 
 class Huge(object):
     def __init__(self):
-        self.QmsgKey = '                            ',
-        self.appid = '                              ',
-        self.apsid = '                              ',
-        # 自动打卡开关，1表示开
-        self.auto = 1,
-
-    def checkin(self):
-        auto = self.auto[0]
-        # 提取id
-        f = open('id.csv', encoding='utf-8')
-        data = f.readline()
-        id = re.sub("\D", "", data)
-        base_url = 'https://apiopen.jingdaka.com/user/get_theme?calendar_id=%s' % (id)
-        headers = {
-            "User-Agent":
-            "Mozilla/5.0 (Linux; Android 11; Mi 10 Pro Build/RKQ1.200826.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/78.0.3904.62 XWEB/2767 MMWEBSDK/201201 Mobile Safari/537.36 MMWEBID/4024 MicroMessenger/8.0.1.1841(0x28000151) Process/appbrand0 WeChat/arm64 Weixin NetType/4G Language/zh_CN ABI/arm64 MiniProgramEnv/android",
-            "content-type": "application/json",
-            "Accept-Encoding": "gzip,compress,br,deflate",
-            "version": "7.3.16",
+        #self.QmsgKey = '',
+        # 抓包获取
+        self.appid = '',
+        self.apsid = '',
+        self.couseid = '',
+        self.session = requests.session()
+        self.headers = {
+            'Host': 'apiopen.jingdaka.com',
+            'Connection': 'keep-alive',
+            'filter': 'test1',
+            'charset': 'utf-8',
             "appid": "%s" % self.appid,
             "apsid": "%s" % self.apsid,
-            "charset": "utf-8",
-            "filter": "test1",
-            "Connection": "keep-alive",
-            "Host": "apiopen.jingdaka.com",
-            "Referer": "https://servicewechat.com/wx5a6e75651505714e/29/page-frame.html"
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 12; Mi 10 Pro Build/SKQ1.220119.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.99 XWEB/3185 MMWEBSDK/20210902 Mobile Safari/537.36 MMWEBID/4024 MicroMessenger/8.0.15.2001(0x28000F41) Process/appbrand0 WeChat/arm64 Weixin GPVersion/1 NetType/4G Language/zh_CN ABI/arm64 MiniProgramEnv/android',
+            'content-type': 'application/json',
+            'Accept-Encoding': 'gzip,compress,br,deflate',
+            'version': '8.5.14',
+            'Referer': 'https://servicewechat.com/%s/36/page-frame.html' % self.appid,
         }
-        session = requests.session()
-        request = session.get(base_url, headers=headers)
-        d = json.loads(request.content.decode('utf8'))
-        sub = d['data']['is_submited']
-        # 正则匹配时间
-        time = d['data']['record_at']
-        real_time = re.match(r'\w{4}-(\w{2}-\w{2})', time)
-        times = real_time.group(1)
-        if sub == 1:
-            text = '今日虎哥打卡任务已完成'
-            print(text)
+
+    def getTimes():
+        return str(datetime.date.today())
+
+    def getCalendarId(self):
+        url = 'https://apiopen.jingdaka.com/user/get_theme'
+        params = (
+            ('course_id', self.couseid),
+            ('record_at', Huge.getTimes()),
+        )
+
+        request = self.session.get(url, headers=self.headers, params=params)
+        data = json.loads(request.content.decode('utf8'))
+        return data['data']['calendar_id']
+
+    def isCheckin(self):
+        url = 'https://apiopen.jingdaka.com/user/get_theme'
+        params = (
+            ('calendar_id', Huge.getCalendarId(self)),
+        )
+
+        request = self.session.get(url, headers=self.headers, params=params)
+        data = json.loads(request.content.decode('utf8'))
+        return data['data']['is_submited']
+
+    def getPic(self):
+        url = 'https://apiopen.jingdaka.com/user/submitlist'
+        params = (
+            ('order_type', '3'),
+            ('limit', '100'),
+            ('offset', '0'),
+            ('search_user_name', '电信'),
+            ('course_id', self.couseid),
+            ('record_at', Huge.getTimes()),
+        )
+
+        request = self.session.get(url, headers=self.headers, params=params)
+        data = json.loads(request.content.decode('utf8'))
+        rand = random.randint(0,99)
+        pic = data['data']['submit_list'][rand]['picture_list'][0]
+        voc = data['data']['submit_list'][rand]['voice_list'][0]
+        return pic, voc
+
+    def checkin(self):
+        if Huge.isCheckin(self) == 1:
+            print('已经打卡了')
+            return
         else:
-            if d['data']['pc_content'] == '':
-                text = '今日没有任务'
-                print(text)
+            url = 'https://apiopen.jingdaka.com/user/submit'
+            data = {
+                "content": "", "word_count": 0, "form_id": "", "document_list": [],
+                "picture_list": [Huge.getPic(self)[0]],
+                "voice_list": [{
+                    "voice": Huge.getPic(self)[1]['voice_url'],
+                    "voice_duration": Huge.getPic(self)[1]['voice_duration']
+                }],
+                "video_list": [],
+                "web_title": "",
+                "website": "",
+                "show_range": 0,
+                "course_calendar_id": Huge.getCalendarId(self),
+                "course_id": self.couseid,
+                "record_at": "%sT00:00:00+08:00" % Huge.getTimes()
+            }
+            request = self.session.post(url, headers=self.headers, data=json.dumps(data))
+            d = json.loads(request.content.decode('utf8'))
+            sub = d['err_msg']
+            if sub == 'SUCCESS':
+                text = '今日英语虎哥未打卡,已为您自动打卡'
             else:
-                text = '今日英语虎哥未打卡'
-                print(text)
-                if auto == 1:
-                    print(id)
-                    url = 'https://apiopen.jingdaka.com/user/submit'
-                    id = int(id)
-                    data2 = {
-                        "content": "", "word_count": 0, "form_id": "", "document_list": [],
-                        "picture_list": ["                                             "],
-                        "voice_list": [
-                            {"voice": "                                          ", "voice_duration": 33}],
-                        "video_list": [], "web_title": "", "website": "", "show_range": 0,
-                        "course_calendar_id": id,
-                        "course_id": 972440, "record_at": "2021-%sT00:00:00+08:00" % (times)
-                    }
-                    session = requests.session()
-                    request = session.get(url, headers=headers,data=json.dumps(data2))
-                    d = json.loads(request.content.decode('utf8'))
-                    sub = d['err_msg']
-                    if sub == 'SUCCESS':
-                        text = '今日英语虎哥未打卡,已为您自动打卡'
-                    else:
-                        text = '今日英语虎哥未打卡,请尽快打卡'
-                QmsgKey = "%s" % self.QmsgKey
-#                 print(QmsgKey)
-#                 print(times)
-#                 print(text)
-                content = f"""{times}{text}"""
-                data = {
-                    "msg": content
-                }
-                url_send = "https://qmsg.zendee.cn/send/%s" % (QmsgKey)
-                print(url_send)
-                try:
-                    res = requests.post(url_send, data=data)
-                    sucmsg = res.json()['success']
-                    if sucmsg == True:
-                        print("qq推送服务成功")
-                    else:
-                        print("qq推送服务失败")
-                except:
-                    print("qq推送参数错误")
-        # 存入id
-        ids = int(id)
-        ids += 1
-        with open('id.csv', 'w', encoding='utf-8') as idFile:
-            idFile.write('%d' % ids)
-        # 日志记录
-        with open('日志.txt', 'a', encoding='utf-8') as idFile:
-            idFile.write('%s,' % times)
-            idFile.write('今日的任务id为%s，' % id)
-            idFile.write('%s\n' % text)
-    
+                text = '今日英语虎哥未打卡,请尽快打卡'
+
+
 if __name__ == '__main__':
     run = Huge()
     run.checkin()
